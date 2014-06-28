e03e19d380aeea25bfbf820c5d7b2cc48786b3e0
pjwl33 <paul.leejw@gmail.com>
2014-06-28 12:22:43 -0400

    form added

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,4 +1,23 @@
 class MealsController < ApplicationController
 
+  def show
+    @meal = Meal.find(params[:id])
+  end
+
+  def new
+    @meal = Meal.new
+  end
+
+  def create
+    @meal = Meal.create(meals_params)
+    if @meal.valid?
+      redirect_to @meal
+    end
+  end
+
+  private
+  def meals_params
+    return params.require(:meal).permit(:comment, :ingredients, :calories, :fat, :protein, :fiber, :sugar, :sodium, :carbs)
+  end
 
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,2 +1,4 @@
 class Meal < ActiveRecord::Base
+
+
 end
