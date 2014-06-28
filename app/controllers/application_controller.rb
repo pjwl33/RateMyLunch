a57a5edb2339daaf325db5d5ac59483acad22e8e
alexshook <alexandra.shook@gmail.com>
2014-06-28 12:23:10 -0400

    adding profile photo, name, goal description to forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_paramaters, if: :devise_controller?
 
   def configure_permitted_paramaters
-    devise_parameter_sanitizer.for(:sign_up) << :name << :goal_description
-    devise_parameter_sanitizer.for(:account_update) << :name << :goal_description
+    devise_parameter_sanitizer.for(:sign_up) << :name << :goal_description << :profile_photo
+    devise_parameter_sanitizer.for(:account_update) << :name << :goal_description << :profile_photo
   end
 
 end
