7ffb00fc19f8e38073561c057fc40a1c070bfd89
alexshook <alexandra.shook@gmail.com>
2014-06-28 12:04:01 -0400

    adding profile photo to users with paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_attached_file :profile_photo, :styles => {  :medium => "300x300>",
+                                                  :thumb => "100x100>" },
+                                                  :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :profile_photo, :content_type => /\Aimage\/.*\Z/
 end
