5adbb1206f0d0b1ccfa80fd22e1519cd6d561c5b
pjwl33 <paul.leejw@gmail.com>
2014-06-28 23:01:50 -0400

    troubleshootign

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -14,6 +14,7 @@ function startVoting(){
   }).done(function(data){
     return data;
   });
+  console.log(meals["responseJSON"]);
   if (meals["responseJSON"].length <= 0){
     $('.vote-template').empty();
     $('.finished-voting').css('visibility', 'visible');
