2db138acc495d6e991483e6d5a21c9ca8841f270
stephenhmarsh <stephenforpresident@gmail.com>
2014-06-28 22:52:07 -0400

    the user show page now has score positioning of all user meals for the week

--- app/models/company.rb
+++ app/models/company.rb
@@ -7,4 +7,12 @@ class Company < ActiveRecord::Base
     last_sunday_night = tonight - tonight.wday
     self.meals.select { |m| m.created_at > last_sunday_night }
   end
+
+  def meals_since_sunday_sorted
+    tonight = DateTime.now.change(hour: 23, min: 59)
+    last_sunday_night = tonight - tonight.wday
+    meals = self.meals.order(rating: :desc)
+    meals.select { |m| m.created_at > last_sunday_night }
+  end
+
 end
