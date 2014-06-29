b0e48844ccd33088567c358eb4246e8a7bba5c9b
jemise111 <jesse.sessler@gmail.com>
2014-06-29 03:00:19 -0400

    update user model meals since sunday method

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,8 +15,7 @@ class User < ActiveRecord::Base
   validates_attachment_content_type :profile_photo, :content_type => /\Aimage\/.*\Z/
 
   def meals_since_sunday
-    tonight = DateTime.now.change(hour: 23, min: 59)
-    last_sunday_night = tonight - tonight.wday
+    last_sunday_night = Date.today.beginning_of_week(:monday).to_datetime.change(hour: 1)
     meals = self.meals.order(created_at: :desc)
     meals.select { |m| m.created_at > last_sunday_night }
   end
