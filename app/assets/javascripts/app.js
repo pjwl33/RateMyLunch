4e0864995c50ec45d4c1bac53068e1624a2a2b5e
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-06-28 20:35:32 -0400

    fixed ajax request to load on DOM

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -6,25 +6,32 @@
 function startVoting(){
   $('.show-template').empty();
   $('.vote-template').css('visibility', 'visible');
-  var meals = grabMeals();
-  $(document).ajaxComplete(function(){
-    // appendMeal(meals[0]);
-    console.log(meals);
-  });
-}
-
-function grabMeals(){
-  $.ajax({
+  var meals = $.ajax({
     method: 'GET',
     url: 'get_meals',
-    dataType: 'JSON'
+    dataType: 'JSON',
+    async: false
   }).done(function(data){
-    console.log(data);
     return data;
-  });
+    });
+  appendMeal(meals["responseJSON"][0]);
 }
 
+// function grabMeals(){
+//   $.ajax({
+//     method: 'GET',
+//     url: 'get_meals',
+//     dataType: 'JSON',
+//     async: false
+//   }).done(function(data){
+//     return data;
+//     });
+// }
+
+
+
 function appendMeal(meal){
+  console.log(meal["id"]);
   $('#vote-meal-pic').append('<p>pic</p>');
   $('#vote-meal-goalname').append('<p><p>').html(meal["goal_id"]);
   $('#vote-meal-comment').append('<p></p>').html(meal["comment"]);
